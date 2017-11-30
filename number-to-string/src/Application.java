d61a058dea27bf5e6efbd84a8d83a86b408119d5
catinthehat13 <32113557+catinthehat13@users.noreply.github.com>
2017-11-30 13:02:59 -0800

    update package name

--- number-to-string/src/Application.java
+++ number-to-string/src/Application.java
@@ -1,10 +1,12 @@
+package numberString;
+
 public static void main(String[] args) 
 {
-		NumberToString app = new NumberToString();
-		
-		app.printIt(100);
-		app.printIt(1234);
-		app.printIt(12304);
-		app.printIt(9);
-		app.printIt(14);
+	NumberToString app = new NumberToString();
+
+	app.printIt(100);
+	app.printIt(1234);
+	app.printIt(12304);
+	app.printIt(9);
+	app.printIt(14);
 }
