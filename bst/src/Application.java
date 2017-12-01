15876fe64a0993d21661af717023327cad90d72a
catinthehat13 <32113557+catinthehat13@users.noreply.github.com>
2017-11-30 16:00:19 -0800

    Create Application.java

---
+++ bst/src/Application.java
@@ -0,0 +1,21 @@
+
+public class Application {
+
+  public static void main(String[] args) {
+
+      BinarySearchTree bst = new BinarySearchTree();
+
+      Scanner in = new Scanner(System.in);
+      String s = in.nextLine();
+      int numNodes = Integer.parseInt(s);
+      s = in.nextLine();
+      String[] values = s.split(" ");
+
+      Node root = null;
+      for (int i = 0; i < numNodes; i++) {
+        int value = Integer.parseInt(values[i]);
+        root = bst.insert(value, root);
+        bst.printTree(root);
+      }
+  }
+}
